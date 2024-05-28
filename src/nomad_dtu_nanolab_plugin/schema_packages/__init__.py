db456fd38a4b583cab36c936e24c3270a1aed8ed
Hampus Näsström <hampus.nasstrom@gmail.com>
2024-05-28 11:16:13 +0200

    Added sputtering and gas schema packages

--- src/nomad_dtu_nanolab_plugin/schema_packages/__init__.py
+++ src/nomad_dtu_nanolab_plugin/schema_packages/__init__.py
@@ -6,7 +6,8 @@ class MySchemaPackageEntryPoint(SchemaPackageEntryPoint):
     parameter: int = Field(0, description='Custom configuration parameter')
 
     def load(self):
-        from nomad_dtu_nanolab_plugin.schema_packages.mypackage import m_package
+        from nomad_dtu_nanolab_plugin.schema_packages.mypackage import \
+            m_package
 
         return m_package
 
@@ -14,4 +15,33 @@ class MySchemaPackageEntryPoint(SchemaPackageEntryPoint):
 mypackage = MySchemaPackageEntryPoint(
     name='MyPackage',
     description='Schema package defined using the new plugin mechanism.',
+)
+
+
+class SputteringEntryPoint(SchemaPackageEntryPoint):
+
+    def load(self):
+        from nomad_dtu_nanolab_plugin.schema_packages.sputtering import \
+            m_package
+
+        return m_package
+
+
+sputtering = MySchemaPackageEntryPoint(
+    name='Sputtering',
+    description='Schema package defined for sputtering.',
+)
+
+
+class GasEntryPoint(SchemaPackageEntryPoint):
+
+    def load(self):
+        from nomad_dtu_nanolab_plugin.schema_packages.gas import m_package
+
+        return m_package
+
+
+gas = MySchemaPackageEntryPoint(
+    name='Gas',
+    description='Schema package defined for gas.',
 )
