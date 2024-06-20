4b34c1a8b9c673f91b11f5fbdd15be9f70accdd5
LeAnMi <154213869+LeAnMi@users.noreply.github.com>
2024-06-20 09:52:11 +0200

    test2

--- src/nomad_dtu_nanolab_plugin/schema_packages/sputtering.py
+++ src/nomad_dtu_nanolab_plugin/schema_packages/sputtering.py
@@ -20,12 +20,15 @@ from typing import TYPE_CHECKING
 
 import numpy as np
 from nomad.datamodel.data import ArchiveSection, Schema
-from nomad.datamodel.metainfo.annotations import ELNAnnotation, ELNComponentEnum
+from nomad.datamodel.metainfo.annotations import (ELNAnnotation,
+                                                  ELNComponentEnum)
 from nomad.datamodel.metainfo.basesections import CompositeSystemReference
 from nomad.metainfo import MEnum, Package, Quantity, Section, SubSection
-from nomad_material_processing.vapor_deposition import ChamberEnvironment, GasFlow
+from nomad_material_processing.vapor_deposition import (ChamberEnvironment,
+                                                        GasFlow)
 from nomad_material_processing.vapor_deposition.pvd import PVDSource, PVDStep
-from nomad_material_processing.vapor_deposition.pvd.sputtering import SputterDeposition
+from nomad_material_processing.vapor_deposition.pvd.sputtering import \
+    SputterDeposition
 
 from nomad_dtu_nanolab_plugin.categories import DTUNanolabCategory
 from nomad_dtu_nanolab_plugin.schema_packages.gas import DTUGasSupply
@@ -84,13 +87,13 @@ class Chamber(ArchiveSection):
     )
     applied_RF_bias_platen = Quantity(
         type=np.float64,
-        default= 0
+        default= 0,
         a_eln={'component': 'NumberEditQuantity', 'defaultDisplayUnit': 'V'},
         unit='V',
     )
     total_pressure = Quantity(
         type=np.float64,
-        default= 5
+        default= 5,
         a_eln={'component': 'NumberEditQuantity', 'defaultDisplayUnit': 'mtorr'},
         unit='kg/(m*s^2)',
     )
