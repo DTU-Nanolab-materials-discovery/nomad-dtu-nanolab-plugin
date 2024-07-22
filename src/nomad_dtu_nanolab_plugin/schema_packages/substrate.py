c317af694d1e17c60c62e2f6132ce4db2471399f
LeAnMi <154213869+LeAnMi@users.noreply.github.com>
2024-07-22 09:03:20 +0200

    one more run

--- src/nomad_dtu_nanolab_plugin/schema_packages/substrate.py
+++ src/nomad_dtu_nanolab_plugin/schema_packages/substrate.py
@@ -137,19 +137,16 @@ class DTUSubstrate(CompositeSystem, Schema):
             logger (BoundLogger): A structlog logger.
         """
         super().normalize(archive, logger)
-
-
-        if self.edx_data_file is not None:
-
+        if self.edx_data_file:
             import pandas as pd
 
             with archive.m_context.raw_file(self.edx_data_file, 'r') as edx:
                 df = pd.read_excel(edx, sheet_name=0, header=0, engine='openpyxl')
 
-                self.avg_S = df['Layer 1 S Atomic %'].mean()
-                self.avg_P = df['Layer 1 P Atomic %'].mean()
-                self.avg_M1 = df['Layer 1 Cu Atomic %'].mean()
-                self.avg_M2 = 0
+            self.avg_S = df['Layer 1 S Atomic %'].mean()
+            self.avg_P = df['Layer 1 P Atomic %'].mean()
+            self.avg_M1 = df['Layer 1 Cu Atomic %'].mean()
+            self.avg_M2 = 0
             #Extracting the atomic percent from the EDX file, average and populate
 
 
