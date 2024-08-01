3a93862aac6600cec5b4b8776565fbe51b69283b
LeAnMi <154213869+LeAnMi@users.noreply.github.com>
2024-08-01 15:16:07 +0200

    more quantities extracted from the file name

--- src/nomad_dtu_nanolab_plugin/schema_packages/target.py
+++ src/nomad_dtu_nanolab_plugin/schema_packages/target.py
@@ -20,12 +20,11 @@ from typing import TYPE_CHECKING
 
 import numpy as np
 from nomad.datamodel.data import Schema
-from nomad.datamodel.metainfo.annotations import (
-    BrowserAnnotation,
-    ELNAnnotation,
-    ELNComponentEnum,
-)
-from nomad.datamodel.metainfo.basesections import Component, CompositeSystem
+from nomad.datamodel.metainfo.annotations import (BrowserAnnotation,
+                                                  ELNAnnotation,
+                                                  ELNComponentEnum)
+from nomad.datamodel.metainfo.basesections import (Component, CompositeSystem,
+                                                   ElementalComposition)
 from nomad.metainfo import Datetime, Package, Quantity, Section
 
 from nomad_dtu_nanolab_plugin.categories import DTUNanolabCategory
@@ -48,7 +47,6 @@ class DTUTarget(CompositeSystem, Schema):
     )
     supplier_id = Quantity(
         type=str,
-        default='Testbourne',
         a_eln={'component': 'StringEditQuantity'},
     )
     purity = Quantity(
@@ -75,10 +73,6 @@ class DTUTarget(CompositeSystem, Schema):
         default=True,
         a_eln=ELNAnnotation(component=ELNComponentEnum.BoolEditQuantity),
     )
-    target_history = Quantity(
-        type=str,
-        a_eln=ELNAnnotation(component=ELNComponentEnum.RichTextEditQuantity),
-    )
     refill_or_mounting_date = Quantity(
         type=Datetime,
         a_eln={'component': 'DateTimeEditQuantity'},
@@ -102,24 +96,60 @@ class DTUTarget(CompositeSystem, Schema):
         super().normalize(archive, logger)
 
         if self.impurity_file:
+            import os
+            import re
+
             import pandas as pd
+
+            file_name = os.path.basename(self.impurity_file)
+            file_information = file_name.split('_')
+            self.lab_id = (
+                f"{file_information[2]} {file_information[3]} {file_information[4]}"
+            )
+
+            if file_information[3] == 'T':
+                self.supplier_id = 'Testbourne'
+            elif file_information[3] == 'L':
+                self.supplier_id = 'Lesker'
+
+            chemical_formula = file_information[2]
+            # Define the regex pattern to extract elements and their quantities
+            pattern = r'([A-Z][a-z]?)(\d*)'
+            matches = re.findall(pattern, chemical_formula)
+            # Prepare data for DataFrame
+            data = []
+            for match in matches:
+                element = match[0]
+                quantity = int(match[1]) if match[1] else 1
+                data.append([element, quantity])
+            # Create DataFrame
+            df_elements = pd.DataFrame(data, columns=['Element', 'Quantity'])
+            self.elemental_composition = [
+                ElementalComposition() for _ in range(len(df_elements))
+                ]
+            elem_total = df_elements['Quantity'].sum()
+            for i in range(len(df_elements)):
+                self.elemental_composition[i].element = df_elements.iloc[i, 0]
+                self.elemental_composition[i].atomic_fraction = (
+                    df_elements.iloc[i, 1]/elem_total
+                )
 
             with archive.m_context.raw_file(self.impurity_file, 'r') as impurity:
                 df_data = pd.read_csv(impurity, delimiter=' ', header=0)
 
-        self.components = [Component() for _ in range(len(df_data))]
-        df_data = df_data.replace({'<': ''}, regex=True)
+            self.components = [Component() for _ in range(len(df_data))]
+            df_data = df_data.replace({'<': ''}, regex=True)
 
-        for i in range(len(self.components)):
-            self.components[i].name = str(df_data.iloc[i, 0]) + ' impurity'
-            if df_data.iloc[i, 2] == 'ppm':
-                self.components[i].mass_fraction = float(df_data.iloc[i, 1]) * 1e-6
-            elif df_data.iloc[i, 2] == 'wt%':
-                self.components[i].mass_fraction = float(df_data.iloc[i, 1]) * 1e-2
-            elif df_data.iloc[i, 2] == 'ppb':
-                self.components[i].mass_fraction = float(df_data.iloc[i, 1]) * 1e-9
-            else:
-                raise ValueError('The impurity unit is not recognized')
+            for i in range(len(self.components)):
+                self.components[i].name = str(df_data.iloc[i, 0]) + ' impurity'
+                if df_data.iloc[i, 2] == 'ppm':
+                    self.components[i].mass_fraction = float(df_data.iloc[i, 1]) * 1e-6
+                elif df_data.iloc[i, 2] == 'wt%':
+                    self.components[i].mass_fraction = float(df_data.iloc[i, 1]) * 1e-2
+                elif df_data.iloc[i, 2] == 'ppb':
+                    self.components[i].mass_fraction = float(df_data.iloc[i, 1]) * 1e-9
+                else:
+                    raise ValueError('The impurity unit is not recognized')
 
 
 m_package.__init_metainfo__()
