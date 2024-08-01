61ae058a287f9edd074532fa2429c4ceaefc7b1a
LeAnMi <154213869+LeAnMi@users.noreply.github.com>
2024-08-01 15:17:44 +0200

    extracted more parameters from the target file name

--- src/nomad_dtu_nanolab_plugin/schema_packages/target.py
+++ src/nomad_dtu_nanolab_plugin/schema_packages/target.py
@@ -20,11 +20,16 @@ from typing import TYPE_CHECKING
 
 import numpy as np
 from nomad.datamodel.data import Schema
-from nomad.datamodel.metainfo.annotations import (BrowserAnnotation,
-                                                  ELNAnnotation,
-                                                  ELNComponentEnum)
-from nomad.datamodel.metainfo.basesections import (Component, CompositeSystem,
-                                                   ElementalComposition)
+from nomad.datamodel.metainfo.annotations import (
+    BrowserAnnotation,
+    ELNAnnotation,
+    ELNComponentEnum,
+)
+from nomad.datamodel.metainfo.basesections import (
+    Component,
+    CompositeSystem,
+    ElementalComposition,
+)
 from nomad.metainfo import Datetime, Package, Quantity, Section
 
 from nomad_dtu_nanolab_plugin.categories import DTUNanolabCategory
