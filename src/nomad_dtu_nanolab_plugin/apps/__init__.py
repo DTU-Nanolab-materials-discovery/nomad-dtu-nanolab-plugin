a7d0b9f181241ae4b14e470e4a4032de2745e80c
Hampus Näsström <hampus.nasstrom@gmail.com>
2024-11-25 10:34:46 +0100

    Added app dashboard

--- src/nomad_dtu_nanolab_plugin/apps/__init__.py
+++ src/nomad_dtu_nanolab_plugin/apps/__init__.py
@@ -1,12 +1,21 @@
+
+import pkg_resources
+import yaml
 from nomad.config.models.plugins import AppEntryPoint
 from nomad.config.models.ui import (
     App,
     Column,
     Columns,
+    Dashboard,
     FilterMenu,
     FilterMenus,
     Filters,
 )
+
+yaml_file_path = pkg_resources.resource_filename(__name__, 'target_dashboard.yaml')
+
+with open(yaml_file_path) as file:
+    target_dashboard = yaml.safe_load(file)
 
 sputtering_targets= AppEntryPoint(
     name='Sputtering targets app',
@@ -26,14 +35,22 @@ sputtering_targets= AppEntryPoint(
         columns=Columns(
             selected=[
                 'data.lab_id#nomad_dtu_nanolab_plugin.schema_packages.target.DTUTarget',
+                'data.main_material#nomad_dtu_nanolab_plugin.schema_packages.target.DTUTarget',
                 'data.supplier_id#nomad_dtu_nanolab_plugin.schema_packages.target.DTUTarget',
+                'data.refill_or_mounting_date#nomad_dtu_nanolab_plugin.schema_packages.target.DTUTarget',
             ],
             options={
                 'data.lab_id#nomad_dtu_nanolab_plugin.schema_packages.target.DTUTarget': Column( # noqa: E501
                     label='Target ID',
                 ),
+                'data.main_material#nomad_dtu_nanolab_plugin.schema_packages.target.DTUTarget': Column( # noqa: E501
+                    label='Material',
+                ),
                 'data.supplier_id#nomad_dtu_nanolab_plugin.schema_packages.target.DTUTarget': Column( # noqa: E501
                     label='Supplier',
+                ),
+                'data.refill_or_mounting_date#nomad_dtu_nanolab_plugin.schema_packages.target.DTUTarget': Column( # noqa: E501
+                    label='Refill or mounting date',
                 ),
             },
         ),
@@ -45,5 +62,6 @@ sputtering_targets= AppEntryPoint(
         filters_locked={
             'entry_type': 'DTUTarget',
         },
+        dashboard=Dashboard.parse_obj(target_dashboard)
     ),
 )
