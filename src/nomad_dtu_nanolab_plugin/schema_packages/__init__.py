7057e84ef9a952ab0583507e6b6d583d9a2140eb
Hampus Näsström <hampus.nasstrom@gmail.com>
2024-11-28 14:44:35 +0100

    Remove mypackage schema package and related tests

--- src/nomad_dtu_nanolab_plugin/schema_packages/__init__.py
+++ src/nomad_dtu_nanolab_plugin/schema_packages/__init__.py
@@ -1,20 +1,4 @@
 from nomad.config.models.plugins import SchemaPackageEntryPoint
-from pydantic import Field
-
-
-class MySchemaPackageEntryPoint(SchemaPackageEntryPoint):
-    parameter: int = Field(0, description='Custom configuration parameter')
-
-    def load(self):
-        from nomad_dtu_nanolab_plugin.schema_packages.mypackage import m_package
-
-        return m_package
-
-
-mypackage = MySchemaPackageEntryPoint(
-    name='MyPackage',
-    description='Schema package defined using the new plugin mechanism.',
-)
 
 
 class BasesectionsEntryPoint(SchemaPackageEntryPoint):
