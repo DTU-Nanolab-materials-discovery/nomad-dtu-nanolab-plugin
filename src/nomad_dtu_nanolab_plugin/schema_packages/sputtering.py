a23ab1f2b7cbb1ad8dbfe1aee4877a816d61e46c
Eugène Bertin <168723228+eugenebertin@users.noreply.github.com>
2024-12-11 11:12:38 +0100

    fixed formated_log_df,_ = format_logfile(log_df) (wrong unpacking of the values)

--- src/nomad_dtu_nanolab_plugin/schema_packages/sputtering.py
+++ src/nomad_dtu_nanolab_plugin/schema_packages/sputtering.py
@@ -2076,7 +2076,7 @@ class DTUSputtering(SputterDeposition, PlotSection, Schema):
             # Openning the log file
             with archive.m_context.raw_file(self.log_file, 'r') as log:
                 log_df = read_logfile(log.name)
-                formated_log_df = format_logfile(log_df)
+                formated_log_df,_ = format_logfile(log_df)
                 events_plot, params, step_params = read_events(log_df)
             if params is not None:
                 # Writing logfile data to the respective sections
