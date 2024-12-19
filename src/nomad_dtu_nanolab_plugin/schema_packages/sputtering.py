833228b406b0f3d85c451f6d01cbf3a59235b7ea
Eugène Bertin <168723228+eugenebertin@users.noreply.github.com>
2024-12-19 11:00:57 +0100

    fix to not write platen bias if it has not been logged

--- src/nomad_dtu_nanolab_plugin/schema_packages/sputtering.py
+++ src/nomad_dtu_nanolab_plugin/schema_packages/sputtering.py
@@ -1989,10 +1989,10 @@ class DTUSputtering(SputterDeposition, PlotSection, Schema):
         environment.gas_flow.extend(gas_flow)
 
         # write platen bias data
+        if 'platen_bias' in step_params[key]['environment']:
+            platen_bias = self.generate_platen_bias_log_data(step_params, key, logger)
 
-        platen_bias = self.generate_platen_bias_log_data(step_params, key, logger)
-
-        environment.platen_bias = platen_bias
+            environment.platen_bias = platen_bias
 
         # write heater data
 
@@ -2014,6 +2014,7 @@ class DTUSputtering(SputterDeposition, PlotSection, Schema):
         platen_bias.vapor_source.dc_bias = DtuDCBias()
         platen_bias.vapor_source.fwd_power = DtuForwardPower()
         platen_bias.vapor_source.rfl_power = DtuReflectedPower()
+
 
         platen_bias_param_nomad_map = map_platen_bias_params_to_nomad(key)
 
