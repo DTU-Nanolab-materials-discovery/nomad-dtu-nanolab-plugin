e600f3920e79785fc4002ad1c633d24fa612579a
Hampus Näsström <hampus.nasstrom@gmail.com>
2025-02-13 09:57:30 +0100

    Added new apps to toml and fixed some bugs (#104)
    * Added new apps to toml and fixed some bugs
    
    * Removed target ID column from XRD app

--- tests/apps/test_app.py
+++ tests/apps/test_app.py
@@ -1,3 +1,9 @@
 def test_importing_app():
     # this will raise an exception if pydantic model validation fails for th app
-    from nomad_dtu_nanolab_plugin.apps import sputtering_targets  # noqa: F401
+    from nomad_dtu_nanolab_plugin.apps import (  # noqa: F401
+        analysis,
+        edx,
+        sputtering,
+        sputtering_targets,
+        xrd,
+    )
