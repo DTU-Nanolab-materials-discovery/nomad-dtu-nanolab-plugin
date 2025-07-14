026d0ee99f2a7d774eed0725217f9ec3bbecdac9
Inês Rodrigues Diogo <indiogo@dtu.dk>
2025-07-14 16:19:06 +0200

    Extending rtp.py file

--- src/nomad_dtu_nanolab_plugin/schema_packages/rtp.py
+++ src/nomad_dtu_nanolab_plugin/schema_packages/rtp.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numpy as np
 from typing import TYPE_CHECKING
 
 from nomad.datamodel.data import Schema
@@ -6,8 +7,9 @@ from nomad.datamodel.metainfo.annotations import (
     ELNAnnotation,
     ELNComponentEnum,
 )
-
-
+from nomad.datamodel.metainfo.basesections import (
+    Process
+)
 from nomad.datamodel.metainfo.plot import PlotSection
 from nomad.metainfo import Package, Quantity, Section
 from nomad_material_processing.vapor_deposition.cvd.general import (
@@ -50,6 +52,8 @@ class DtuRTP(ChemicalVaporDeposition, PlotSection, Schema):
         ),
         description='The ID of the run. Format: name_number_material_quarter_piece'
         '-totalpieces ',
+        description='The ID of the run. Format: name_number_material_quarter_piece'
+        '-totalpieces ',
     )
     location = Quantity(
         type=str,
@@ -68,10 +72,21 @@ class DtuRTP(ChemicalVaporDeposition, PlotSection, Schema):
         description='Cell to upload the gases log file from the RTP process.',
     )
     log_file_T2BDiagnostics = Quantity(
+    log_file_eklipse = Quantity(
         type=str,
         shape=['*'],
         a_eln=ELNAnnotation(
             component=ELNComponentEnum.FileEditQuantity,
+            label= 'Gases log file'
+        ),
+        description='Cell to upload the gases log file from the RTP process.',
+    )
+    log_file_T2BDiagnostics = Quantity(
+        type=str,
+        shape=['*'],
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.FileEditQuantity,
+            label = 'Temperature log file'
             label = 'Temperature log file'
         ),
         description='Cell to upload the temperature log file from the RTP process.',
@@ -206,13 +221,148 @@ class DtuRTP(ChemicalVaporDeposition, PlotSection, Schema):
 
 ################### ANNEALING PLATEAU ######################
 
-    duration = Quantity(
+        description='Cell to upload the temperature log file from the RTP process.',
+    )
+    #log_file_pressure = Quantity(
+    #    type=str,
+    #    shape=['*'],
+    #    a_eln=ELNAnnotation(
+    #        component=ELNComponentEnum.FileEditQuantity,
+    #        label = 'Pressure log file'
+    #    ),
+    #    description='Cell to upload the pressure log file from the RTP process.',
+    #)
+
+    #################### HEATING RAMPING UP ######################
+
+    uses_toxic_gases = Quantity(
+    type=bool,
+    a_eln=ELNAnnotation(
+        component=ELNComponentEnum.BooleanEditQuantity,
+        label='Are toxic gases used?',
+    ),
+    description='Check if toxic gases are used in the process.',
+)
+    base_pressure = Quantity(
+        type=np.float64,
+        a_eln= ELNAnnotation(
+            component= ELNComponentEnum.FileEditQuantity,
+            label='Base Pressure without ballast',
+            defaultDisplayUnit = 'mtorr'
+            ),
+        unit='Pa',
+        description='Base pressure when ballast is OFF',
+    )
+    base_pressure_ballast = Quantity(
+    type=np.float64,
+    a_eln=ELNAnnotation(
+        component=ELNComponentEnum.FileEditQuantity,
+        label='Base Pressure with ballast',
+        defaultDisplayUnit='mtorr',
+        visibleIf={'uses_toxic_gases': True}  # <- Only visible if toxic gases used
+    ),
+    unit='Pa',
+    description='Base pressure when ballast is ON.',
+)
+    rate_of_rise = Quantity(
         type=np.float64,
         a_eln=ELNAnnotation(
             component=ELNComponentEnum.NumberEditQuantity,
-            defaultDisplayUnit='minute',
+            defaultDisplayUnit='mTorr/minute',
+            label='Rate of Rise',
+        ),
+    unit='Pa/s',
+    description='Rate of rise of the pressure in the RTP chamber during static vacuum',
+    )
+    chiller_flow = Quantity(
+        type=np.float64,
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.NumberEditQuantity,
+            defaultDisplayUnit='l/minute',
+            label='Chiller Flow',
+        ),
+    unit='m³/s',
+    description='Chiller flow rate during the RTP process.',
+    )
+    Ar_flow = Quantity(
+        type=np.float64,
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.NumberEditQuantity,
+            defaultDisplayUnit='sccm',
+            label='Ar Flow',
+            visibleIf={'uses_toxic_gases': False}  # <- Only visible if tox gases not used
+        ),
+        unit='m³/s',
+        description='Argon flow rate used during the RTP process.',
+    )
+    N2_flow = Quantity(
+        type=np.float64,
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.NumberEditQuantity,
+            defaultDisplayUnit='sccm',
+            label='N2 Flow',
+            visibleIf={'uses_toxic_gases': False}  # <- Only visible if tox gases not used
+        ),
+        unit='m³/s',
+        description='Nitrogen flow rate used during the RTP process.',
+    )
+    PH3_flow = Quantity(
+        type=np.float64,
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.NumberEditQuantity,
+            defaultDisplayUnit='sccm',
+            label='PH3 Flow',
+            visibleIf={'uses_toxic_gases': True}  # <- Only visible if toxic gases used
+        ),
+        unit='m³/s',
+        description='Phosphine flow rate used during the RTP process.',
+    )
+    H2S_flow = Quantity(
+        type=np.float64,
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.NumberEditQuantity,
+            defaultDisplayUnit='sccm',
+            label='H2S Flow',
+            visibleIf={'uses_toxic_gases': True}  # <- Only vis if toxic gases used
+        ),
+        unit='m³/s',
+        description='H2S flow rate used during the RTP process.',
+    )
+    heating_up_rate = Quantity(
+        type=np.float64,
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.NumberEditQuantity,
+            defaultDisplayUnit='Celsius/minute',
+            label='Heating Up Rate',
+        ),
+    unit='K/s',
+    description='Rate of temperature increase during the heating up phase of the' \
+    ' RTP process.',
+    )
+    heating_up_pressure = Quantity(
+        type=np.float64,
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.NumberEditQuantity,
+            defaultDisplayUnit='Torr',
+            label='HEating up pressure',
+        ),
+    unit='Pa',
+    description='Pressure in the RTP chamber during the heating up phase of the' \
+    ' RTP process.',
+    )
+
+################### ANNEALING PLATEAU ######################
+
+    duration = Quantity(
+        type=np.float64,
+        type=np.float64,
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.NumberEditQuantity,
+            defaultDisplayUnit='minute'
             label= 'Duration',
         ),
+        unit='s',
+        description='Duration of the plateau',
         unit='s',
         description='Duration of the plateau',
     )
@@ -222,6 +372,7 @@ class DtuRTP(ChemicalVaporDeposition, PlotSection, Schema):
             component=ELNComponentEnum.NumberEditQuantity,
             defaultDisplayUnit='degree_Celsius',
         ),
+        unit='K',
         unit='K',
         description='Temperature of the process.',
     )
