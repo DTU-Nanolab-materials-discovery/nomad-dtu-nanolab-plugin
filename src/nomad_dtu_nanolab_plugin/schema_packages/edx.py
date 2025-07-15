05933f86ca09ab01a7886ef8770f3628219f4f73
LeAnMi <154213869+LeAnMi@users.noreply.github.com>
2025-07-15 10:38:50 +0200

    Revert "test from a different branch"
    This reverts commit 25d0dcf04c2f55ab45284141d5388f175ded001b.

--- src/nomad_dtu_nanolab_plugin/schema_packages/edx.py
+++ src/nomad_dtu_nanolab_plugin/schema_packages/edx.py
@@ -65,17 +65,6 @@ class EDXResult(MappingResult):
         ),
         unit='m',
     )
-    assumed_material_density = Quantity(
-        type=np.float64,
-        description="""
-        The assumed material density for the thickness determination.
-        """,
-        a_eln=ELNAnnotation(
-            component=ELNComponentEnum.NumberEditQuantity,
-            defaultDisplayUnit='g/cm^3',
-        ),
-        unit='kg/m^3',
-    )
     quantifications = SubSection(
         section_def=EDXQuantification,
         repeats=True,
@@ -138,17 +127,6 @@ class EDXMeasurement(MappingMeasurement, PlotSection, Schema):
             defaultDisplayUnit='nm',
         ),
         unit='m',
-    )
-    avg_density = Quantity(
-        type=np.float64,
-        description="""
-            The assumed material density for the thickness determination
-                            """,
-        a_eln=ELNAnnotation(
-            component=ELNComponentEnum.NumberEditQuantity,
-            defaultDisplayUnit='g/cm^3',
-        ),
-        unit='kg/m^3',
     )
     results = SubSection(
         section_def=EDXResult,
@@ -441,9 +419,6 @@ class EDXMeasurement(MappingMeasurement, PlotSection, Schema):
         avg_layer_thickness = ureg.Quantity(
             df_data['Layer 1 Thickness (nm)'].mean(), 'nm'
         )
-        avg_density = ureg.Quantity(
-            df_data['Layer 1 Density (g/cm^3)'].mean(), 'g/cm^3'
-        )
 
         pattern = r'Layer 1 [A-Z][a-z]? Atomic %'
         percentage_labels = [
@@ -463,9 +438,6 @@ class EDXMeasurement(MappingMeasurement, PlotSection, Schema):
                 x_absolute=ureg.Quantity(row['X (mm)'], 'mm'),
                 y_absolute=ureg.Quantity(row['Y (mm)'], 'mm'),
                 layer_thickness=ureg.Quantity(row['Layer 1 Thickness (nm)'], 'nm'),
-                assumed_material_density=ureg.Quantity(
-                    row['Layer 1 Density (g/cm^3)'], 'g/cm^3'
-                ),
                 quantifications=quantifications,
             )
             result.normalize(archive, logger)
@@ -473,7 +445,6 @@ class EDXMeasurement(MappingMeasurement, PlotSection, Schema):
         edx = EDXMeasurement(
             results=results,
             avg_layer_thickness=avg_layer_thickness,
-            avg_density=avg_density,
             sample_alignment=sample_alignment,
         )
         merge_sections(self, edx, logger)
