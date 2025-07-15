a15c3aaa8866fb46e717e5cb2decdbd0c9449529
Hampus Näsström <hampus.nasstrom@gmail.com>
2025-07-15 09:39:38 +0200

    Added schema for thermal evaporation (#147)
    * Added schema for thermal evaporation and RTP entry point to toml
    
    * Added CVD inheritance for rtp
    
    * Add ontology link to RTP

---
+++ src/nomad_dtu_nanolab_plugin/schema_packages/thermal.py
@@ -0,0 +1,16 @@
+from nomad.datamodel.data import Schema
+from nomad.metainfo.metainfo import Package, Section
+from nomad_material_processing.vapor_deposition.pvd.thermal import ThermalEvaporation
+
+from nomad_dtu_nanolab_plugin.categories import DTUNanolabCategory
+
+m_package = Package()
+
+class DtuThermalEvaporation(ThermalEvaporation, Schema):
+    m_def = Section(
+        categories=[DTUNanolabCategory],
+        label='Bell Jar Evaporator',
+    )
+
+
+m_package.__init_metainfo__()
