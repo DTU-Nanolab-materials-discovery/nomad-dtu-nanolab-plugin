516f68095e3f02388e1690a2769c1978201c367e
LeAnMi <154213869+LeAnMi@users.noreply.github.com>
2025-07-15 10:38:50 +0200

    Revert "test from a different branch"
    This reverts commit 25d0dcf04c2f55ab45284141d5388f175ded001b.

--- src/nomad_dtu_nanolab_plugin/schema_packages/substrate.py
+++ src/nomad_dtu_nanolab_plugin/schema_packages/substrate.py
@@ -23,6 +23,9 @@ from nomad_dtu_nanolab_plugin.categories import DTUNanolabCategory
 from nomad_dtu_nanolab_plugin.schema_packages.sample import (
     DTUCombinatorialLibrary, DtuLibraryReference)
 
+
+
+
 if TYPE_CHECKING:
     from nomad.datamodel.datamodel import EntryArchive
     from structlog.stdlib import BoundLogger
