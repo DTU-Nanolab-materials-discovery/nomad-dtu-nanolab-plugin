0b508d26981b22198120f5bf171518703bfe298a
Eugène Bertin <168723228+eugenebertin@users.noreply.github.com>
2025-07-31 15:56:12 +0000

    faulty line

--- src/nomad_dtu_nanolab_plugin/schema_packages/sample.py
+++ src/nomad_dtu_nanolab_plugin/schema_packages/sample.py
@@ -421,7 +421,7 @@ class ProcessParameterOverview(ArchiveSection):
         ),
         unit='m',
     )
-    deposition_parameters = SubSection(
+    deposition_parameters = SubSection(  # FAULTY LINE
         section_def=DepositionParameters,
     )
 
@@ -432,7 +432,7 @@ class DTUCombinatorialLibrary(CombinatorialLibrary, ThinFilmStack, Schema):
         label='Combinatorial Library',
     )
 
-    process_parameter_overview = SubSection(  # FAULTY LINE
+    process_parameter_overview = SubSection(
         section_def=ProcessParameterOverview,
     )
 
