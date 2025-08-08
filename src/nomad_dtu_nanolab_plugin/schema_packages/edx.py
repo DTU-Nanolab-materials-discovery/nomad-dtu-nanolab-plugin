9f475af81e4ac98a91926848c2b5ff02e48aeaca
LeAnMi <154213869+LeAnMi@users.noreply.github.com>
2025-08-08 14:59:01 +0200

    make density more flexible in handling

--- src/nomad_dtu_nanolab_plugin/schema_packages/edx.py
+++ src/nomad_dtu_nanolab_plugin/schema_packages/edx.py
@@ -441,9 +441,13 @@ class EDXMeasurement(MappingMeasurement, PlotSection, Schema):
         avg_layer_thickness = ureg.Quantity(
             df_data['Layer 1 Thickness (nm)'].mean(), 'nm'
         )
-        avg_density = ureg.Quantity(
-            df_data['Layer 1 Density (g/cm³)'].mean(), 'g/(cm**3)'
-        )
+        if self.avg_density is None:
+            if 'Layer 1 Density (g/cm³)' in df_data.columns:
+                avg_density = ureg.Quantity(
+                    df_data['Layer 1 Density (g/cm³)'].mean(), 'g/(cm**3)'
+                )
+        else:
+            avg_density = ureg.Quantity(self.avg_density, 'g/(cm**3)')
 
         pattern = r'Layer 1 [A-Z][a-z]? Atomic %'
         percentage_labels = [
@@ -463,11 +467,17 @@ class EDXMeasurement(MappingMeasurement, PlotSection, Schema):
                 x_absolute=ureg.Quantity(row['X (mm)'], 'mm'),
                 y_absolute=ureg.Quantity(row['Y (mm)'], 'mm'),
                 layer_thickness=ureg.Quantity(row['Layer 1 Thickness (nm)'], 'nm'),
-                assumed_material_density=ureg.Quantity(
-                    row['Layer 1 Density (g/cm³)'], 'g/(cm**3)'
-                ),
                 quantifications=quantifications,
             )
+
+            if 'Layer 1 Density (g/cm³)' in df_data.columns:
+                    result.assumed_material_density = ureg.Quantity(
+                        row['Layer 1 Density (g/cm³)'], 'g/(cm**3)'
+                    )
+            elif self.avg_density is not None:
+                result.assumed_material_density = avg_density
+
+
             result.normalize(archive, logger)
             results.append(result)
         edx = EDXMeasurement(
