a830bd3ded33cb7d460ab3e8eeb786cefee4b830
LeAnMi <154213869+LeAnMi@users.noreply.github.com>
2025-08-12 12:12:10 +0200

    next reference test

--- src/nomad_dtu_nanolab_plugin/schema_packages/substrate.py
+++ src/nomad_dtu_nanolab_plugin/schema_packages/substrate.py
@@ -871,7 +871,7 @@ class DTULibraryCleaving(Process, Schema, PlotSection):
 
         origin= self.combinatorial_Library
         origin_ref= DtuLibraryReference(
-            reference=origin,
+            #reference=origin,
             name=origin.name,
             lab_id=origin.lab_id,
         )
