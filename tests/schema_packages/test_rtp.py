d57e7ec4c33cf864ee0c1a406171f9591e58a18a
Inês Rodrigues Diogo <indiogo@dtu.dk>
2025-09-16 14:58:18 +0200

    added rtp_test.py, small fix in yaml rtp test file, small fixes in logic in rtp.py and added order to subsections.

---
+++ tests/schema_packages/test_rtp.py
@@ -0,0 +1,14 @@
+import os.path
+
+import pytest
+from nomad.client import normalize_all, parse
+
+
+def test_schema():
+    test_file = os.path.join('tests', 'data', 'test_rtp_2.archive.yaml')
+    entry_archive = parse(test_file)[0]
+    normalize_all(entry_archive)
+
+    assert entry_archive.data.steps[0].step_overview.temperature_ramp == pytest.approx(
+        91.6667
+    )
