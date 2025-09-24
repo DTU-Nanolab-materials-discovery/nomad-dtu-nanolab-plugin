fad76eb1d01c0ee6e9d0a71146e1ad95056d95fd
Eugène Bertin <168723228+eugenebertin@users.noreply.github.com>
2025-09-24 11:18:20 +0200

    fix: correct import statement for 'true' in sputtering.py

--- src/nomad_dtu_nanolab_plugin/schema_packages/sputtering.py
+++ src/nomad_dtu_nanolab_plugin/schema_packages/sputtering.py
@@ -17,12 +17,12 @@
 #
 
 import json
-import true
 from datetime import datetime
 from typing import TYPE_CHECKING, Self
 
 import numpy as np
 import pandas as pd
+import true
 from nomad.datamodel.data import ArchiveSection, Schema
 from nomad.datamodel.metainfo.annotations import (
     BrowserAdaptors,
