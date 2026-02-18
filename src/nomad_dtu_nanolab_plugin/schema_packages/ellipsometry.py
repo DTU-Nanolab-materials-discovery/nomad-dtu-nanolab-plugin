7e42d0a827f0a1ea922093f76bc3e95f50b4af69
eugenebertin <168723228+eugenebertin@users.noreply.github.com>
2026-02-18 14:52:20 +0100

    ruff

--- src/nomad_dtu_nanolab_plugin/schema_packages/ellipsometry.py
+++ src/nomad_dtu_nanolab_plugin/schema_packages/ellipsometry.py
@@ -28,7 +28,6 @@ from nomad.metainfo import Package, Quantity, Section, SubSection
 from nomad.units import ureg
 from nomad_measurements.mapping.schema import (
     MappingResult,
-    RectangularSampleAlignment,
 )
 from nomad_measurements.utils import merge_sections
 from scipy.interpolate import griddata
@@ -36,6 +35,7 @@ from structlog.stdlib import BoundLogger
 
 from nomad_dtu_nanolab_plugin.categories import DTUNanolabCategory
 from nomad_dtu_nanolab_plugin.schema_packages.basesections import (
+    DTUBaseSampleAlignment,
     DtuNanolabMeasurement,
 )
 
