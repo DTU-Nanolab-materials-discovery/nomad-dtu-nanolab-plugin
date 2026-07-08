3ddbabc3ceb3489fb3ebda561bdc16761576bf40
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2026-07-08 11:50:59 +0200

    Fix: use nomad logger in test_parse_file

--- tests/parsers/test_parser.py
+++ tests/parsers/test_parser.py
@@ -1,6 +1,5 @@
-import logging
-
 from nomad.datamodel import EntryArchive
+from nomad.utils import get_logger
 
 from nomad_dtu_nanolab_plugin.parsers.myparser import MyParser
 
@@ -8,6 +7,6 @@ from nomad_dtu_nanolab_plugin.parsers.myparser import MyParser
 def test_parse_file():
     parser = MyParser()
     archive = EntryArchive()
-    parser.parse('tests/data/example.out', archive, logging.getLogger())
+    parser.parse('tests/data/example.out', archive, get_logger(__name__))
 
     assert archive.results.material.elements == ['H', 'O']
